dc6cc218015febb2ec485a25cb08c08fa319c8e8
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-15 19:30:24 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
           <div className="opacity-75 transition hover:opacity-100 cursor-pointer">
             <Link href="https://hearti.co" className="flex items-center justify-center">
               <p className="text-base mr-2">Hearti.ai Embeddings Starter</p>
-              <Image src={'/supabase.svg'} width="20" height="20" alt="Supabase logo" />
+              <Image src={'/hearty2.svg'} width="20" height="20" alt="Hearti logo" />
             </Link>
           </div>
           <div className="border-l border-gray-300 w-1 h-4" />
